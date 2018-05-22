763c4c3f210fff8d2ec23c2f5970df875e811c32
Mariana <marianagladii@gmail.com>
2018-05-22 16:53:56 +0300

    Marianachange

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,17 +8,19 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("\nWhat is your name? ");
-            var name = Console.ReadLine();
+            var name1 = Console.ReadLine();
+
             var date = DateTime.Now;
             Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
             Console.Write("\nPress any key to exit...");
             Console.ReadKey(true);
 
+
 		//yulia comment
 
 //mariana coment
 
-                //tanya comment
+                
 
 
  
